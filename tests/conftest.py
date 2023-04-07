281a1192533a0e8ba85ae6a5d85d4d8fdc533fcc
Schamper <1254028+Schamper@users.noreply.github.com>
2023-04-07 14:23:33 +0200

    Add type hints

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,19 @@
 import gzip
 import os
+from typing import BinaryIO, Iterator
 
 import pytest
 
 
-def absolute_path(filename):
+def absolute_path(filename) -> str:
     return os.path.join(os.path.dirname(__file__), filename)
 
 
-def gzip_file(filename):
+def gzip_file(filename) -> Iterator[BinaryIO]:
     with gzip.GzipFile(absolute_path(filename), "rb") as fh:
         yield fh
 
 
 @pytest.fixture
-def ext4_simple():
+def ext4_simple() -> Iterator[BinaryIO]:
     yield from gzip_file("data/ext4.bin.gz")
